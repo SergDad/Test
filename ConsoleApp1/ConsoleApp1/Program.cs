b125c60079d61ca0240c3475e2db0f13312aef83
SergDadFork <dadkof@mail.ru>
2024-05-24 12:44:12 +0500

    Commit#1
    Первый комит 2024.05.24

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,47 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using ConsoleApp2;
+
+Console.WriteLine("Hello, World!");
+var test = (float)4;
+var test2 = test + 3;
+var test3 = test + 3;
+var aa = new Dictionary<string, string>();
+var a = new Test();
+a.GetHashCode();
+var b = 2;
+var c = 4;
+
+var person = new Person("f");
+var person1 = new Person("d");
+var person12 = new Person("e");
+var person1232 = new Person("f");
+person.ShowLastName();
+
+foreach (var e in Person.Persons)
+{
+    e.ShowLastName();
+}
+
+class Person
+{
+    public static List<Person> Persons { get; set; } = new();
+    
+    public Person()
+    {
+        Persons.Add(this);
+    }
+    
+    public Person(string lastName)
+    {
+        this.LastName = lastName;
+        Persons.Add(this);
+    }
+    
+    public string LastName { get; set; }
+
+    public void ShowLastName()
+    {
+        Console.WriteLine(LastName);
+    }
+}
